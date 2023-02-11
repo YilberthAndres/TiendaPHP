8206365f12305dba661583031831d9d84aa5659f
Yilberth Andres <89434567+YilberthAndres@users.noreply.github.com>
2023-02-11 09:08:22 -0500

    index

--- index.php
+++ index.php
@@ -1,14 +1,1 @@
-<?php
-// require_once('view/head/head.php'); 
-    // if(empty($_SESSION['usuario'])){
-        header("Location:view/home/login.php");
-    // }
-?>
-<?php 
-        
-?>
-
-    <!-- <a href="view/persona/create.php" class="btn btn-primary"> Agregar Usuario</a> -->
-<?php 
-        // require_once('view/head/footer.php'); 
-?>
+<?php header("Location:view/home/login.php"); ?>
